efe0c4f68d3da655279835391de146d523002a57
DeepGajjar7 <93926192+DeepGajjar7@users.noreply.github.com>
2022-05-22 23:59:09 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+x = 0;
+y = 0;
+
+draw_apple = "";
+
+var SpeechRecognition = window.webkitSpeechRecognition;
+  
+var recognition = new SpeechRecognition();
+
+function start()
+{
+  document.getElementById("status").innerHTML = "System is listening please speak";  
+  recognition.start();
+} 
+ 
+recognition.onresult = function(event) {
+
+ console.log(event); 
+
+ content = event.results[0][0].transcript;
+
+    document.getElementById("status").innerHTML = "The speech has been recognized: " + content; 
+
+}
+
+function setup() {
+  rceateCanvas(900,600)
+}
+
+function draw() {
+  if (drawapple=="set"){
+    apple(x,y,radius);
+    document.getElementById("status").innerHTML="Apple Is Drawn"
+    drawapple=""
+}
+}
+
+function speak(){
+    var synth = window.speechSynthesis;
+
+    var utterThis = new SpeechSynthesisUtterance(speak_data);
+
+    synth.speak(utterThis);
+
+    speak_data = "";
+}
